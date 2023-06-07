8d7c0a2b5f848f23ab55ad768c82712fdb577834
Lee Zuo An <1211205451@student.mmu.edu.my>
2023-06-07 20:40:23 +0800

    an

--- main_menu.py
+++ main_menu.py
@@ -7,6 +7,40 @@ def user_displayMenu():#这边是改user menu 的
     print("4.Genting Highlands	4D3N	RM 350.00 RM 245.00")
     print("5.Cameron Highlands	4D3N	RM 300.00 RM 210.00")
 
+def display_menu():
+    print("==== Customer Menu ====")
+    print("1. Add Customer")
+    print("2. Remove Customer")
+    print("3. View Customers")
+    print("4. Exit")
+
+customers = []
+
+while True:
+    display_menu()
+    choice = input("Enter your choice (1-4): ")
+
+    if choice == "1":
+        name = input("Enter customer name: ")
+        customers.append(name)
+        print("Customer added successfully!")
+    elif choice == "2":
+        name = input("Enter customer name to remove: ")
+        if name in customers:
+            customers.remove(name)
+            print("Customer removed successfully!")
+        else:
+            print("Customer not found!")
+    elif choice == "3":
+        print("Customer List:")
+        for customer in customers:
+            print(customer)
+    elif choice == "4":
+        print("Exiting...")
+        break
+    else:
+        print("Invalid choice! Please enter a number from 1 to 4.")
+
 def admin_displayMenu():#这边是改admin menu 的
     print("\nhahahah admin")
 
