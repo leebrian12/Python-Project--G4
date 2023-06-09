f16e72e57eceb7c7c1696355d7d2f1c4565c5576
TehKaiChun <128765942+TehKaiChun@users.noreply.github.com>
2023-06-09 12:19:34 +0800

    Update package.py

--- package.py
+++ package.py
@@ -1,78 +1,100 @@
-class TravelPackage:
-    def __init__(self, name, destinations, price):
-        self.name = name
-        self.destinations = destinations
-        self.price = price
+import ast
+import os
 
+PACKAGE_FILE = 'travel_packages.txt'
 
-class TravelPackageManager:
-    def __init__(self):
-        self.packages = []
+def save_packages(packages):
+    with open(PACKAGE_FILE, 'w') as file:
+        file.write(str(packages))
 
-    def add_package(self):
-        name = input("Enter the package name: ")
-        destinations = input("Enter the destinations (comma-separated): ").split(",")
-        price = float(input("Enter the package price: "))
-        package = TravelPackage(name, destinations, price)
-        self.packages.append(package)
-        print(f"Package '{name}' added successfully.")
+def load_packages():
+    try:
+        with open(PACKAGE_FILE, 'r') as file:
+            content = file.read()
+            if content:
+                return ast.literal_eval(content)
+    except FileNotFoundError:
+        return []
 
-    def edit_package(self):
-        name = input("Enter the package name to edit: ")
-        for package in self.packages:
-            if package.name == name:
-                new_name = input("Enter the new package name: ")
-                new_destinations = input("Enter the new destinations (comma-separated): ").split(",")
-                new_price = float(input("Enter the new package price: "))
-                package.name = new_name
-                package.destinations = new_destinations
-                package.price = new_price
-                print(f"Package '{name}' updated successfully.")
-                return
-        print(f"Package '{name}' not found.")
-
-    def delete_package(self):
-        name = input("Enter the package name to delete: ")
-        for package in self.packages:
-            if package.name == name:
-                self.packages.remove(package)
-                print(f"Package '{name}' deleted successfully.")
-                return
-        print(f"Package '{name}' not found.")
-
-    def display_packages(self):
-        if self.packages:
-            print("Travel Packages:")
-            for package in self.packages:
-                print(f"- {package.name}: Destinations: {', '.join(package.destinations)}, Price: {package.price}")
-        else:
-            print("No travel packages found.")
-
-
-# Example usage
-manager = TravelPackageManager()
-
-while True:
-    print("\nMenu:")
-    print("1. Add Travel Package")
-    print("2. Edit Travel Package")
-    print("3. Delete Travel Package")
-    print("4. Display Travel Packages")
-    print("5. Exit")
-
-    choice = input("Enter your choice (1-5): ")
-
-    if choice == "1":
-        manager.add_package()
-    elif choice == "2":
-        manager.edit_package()
-    elif choice == "3":
-        manager.delete_package()
-    elif choice == "4":
-        manager.display_packages()
-    elif choice == "5":
-        break
+def display_packages(packages):
+    print("Travel Packages:")
+    if packages:
+        for i, package in enumerate(packages, start=1):
+            print(f"{i}. Name: {package['name']}, Destination: {package['destination']}, Price: {package['price']}")
     else:
-        print("Invalid choice. Please try again.")
+        print("No travel packages found.")
+        
+def add_package(packages):
+    name = input("Enter package name: ")
+    destination = input("Enter destination: ")
+    price = input("Enter price: ")
+    package = {"name": name, "destination": destination, "price": price}
+    packages.append(package)
+    save_packages(packages)
+    print("Package added successfully!")
 
-print("Program exited.")
+def display_packages(packages):
+    print("Travel Packages:")
+    for i, package in enumerate(packages, start=1):
+        print(f"{i}. Name: {package['name']}, Destination: {package['destination']}, Price: {package['price']}")
+
+def edit_package(packages):
+    display_packages(packages)
+    index = int(input("Enter the index of the package to edit: ")) - 1
+    if 0 <= index < len(packages):
+        package = packages[index]
+        print("Enter new values (leave blank to keep the existing value):")
+        new_name = input(f"Name ({package['name']}): ") or package['name']
+        new_destination = input(f"Destination ({package['destination']}): ") or package['destination']
+        new_price = input(f"Price ({package['price']}): ") or package['price']
+        package['name'] = new_name
+        package['destination'] = new_destination
+        package['price'] = new_price
+        save_packages(packages)
+        print("Package edited successfully!")
+    else:
+        print("Invalid package index!")
+
+def delete_package(packages):
+    display_packages(packages)
+    index = int(input("Enter the index of the package to delete: ")) - 1
+    if 0 <= index < len(packages):
+        package = packages[index]
+        packages.remove(package)
+        save_packages(packages)
+        print("Package deleted successfully!")
+    else:
+        print("Invalid package index!")
+
+def main():
+    current_directory = os.getcwd()
+    abs_package_file_path = os.path.join(current_directory, PACKAGE_FILE)
+    print("Travel package file path:", abs_package_file_path)
+
+    packages = load_packages()
+    while True:
+        print("\n------ Travel Package Management ------")
+        print("1. Add Package")
+        print("2. Edit Package")
+        print("3. Delete Package")
+        print("4. View Packages")
+        print("5. Exit")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            add_package(packages)
+        elif choice == "2":
+            edit_package(packages)
+        elif choice == "3":
+            delete_package(packages)
+        elif choice == "4":
+            display_packages(packages)
+        elif choice == "5":
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == '__main__':
+    # Check if the package file exists, otherwise create it
+    if not os.path.isfile(PACKAGE_FILE):
+        save_packages([])
+    main()
