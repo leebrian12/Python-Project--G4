4b5f529e8729ab95f1020a06ec734e117887cdc4
Lee Zuo An <1211205451@student.mmu.edu.my>
2023-06-10 13:46:36 +0800

    an

--- CustomerMenu.py
+++ CustomerMenu.py
@@ -1,79 +1,64 @@
-import ast
+import os
 
-def main():
+def display_packages():
+    print("Available Travel Packages:")
+    with open("travel_packages.txt", "r") as file:
+        packages = file.readlines()
+        for package in packages:
+            print(package.strip())
 
-    display_menu()
+def view_cart():
+    print("Your Cart:")
+    with open("cart.txt", "r") as file:
+        items = file.readlines()
+        for item in items:
+            print(item.strip())
 
-    # Create an empty cart for the customer
-    cart = []
+def add_to_cart(package_name):
+    with open("cart.txt", "a") as file:
+        file.write(package_name + "\n")
+    print(package_name + " added to cart.")
 
-    # Prompt the user for input and process their choice
-    while True:
-        choice = input("Enter your choice: ")
+def remove_from_cart(package_name):
+    with open("cart.txt", "r") as file:
+        items = file.readlines()
+    items = [item.strip() for item in items if item.strip() != package_name]
+    with open("cart.txt", "w") as file:
+        file.write("\n".join(items))
+    print(package_name + " removed from cart.")
 
-        if choice == "1":
-            check_package()
-        elif choice == "2":
-            view_cart(cart)
-        elif choice == "3":
-            add_to_cart(cart)
-        elif choice == "4":
-            remove_from_cart(cart)
-        elif choice == "5":
-           calculate_bill(cart)
-        elif choice == "6":
-            break
-        else:
-            print("Invalid choice. Please try again.")
+def make_payment():
+    print("Payment processed. Thank you for your purchase!")
+if not os.path.exists("cart.txt"):
+    with open("cart.txt", "w") as file:
+        file.write("")
 
-    cleanup_system()
-
-def display_menu():
-    print("===== Travel Management System =====")
-    print("1. Check Package")
+while True:
+    print("\n--- Travel Package Store ---")
+    print("1. View Packages")
     print("2. View Cart")
-    print("3. Add Item to Cart")
-    print("4. Remove Item from Cart")
-    print("5. Payment")
+    print("3. Add to Cart")
+    print("4. Remove from Cart")
+    print("5. Make Payment")
     print("6. Exit")
 
-def check_package(package_name):
-    # Assume we have a list of available packages
-    available_packages = [
-    package1 = TravelPackage("Package A", "Pulau Langkawi", 150.00)
-    package2 = TravelPackage("Package B", "Pulau Redang", 120.00)
-    package3 = TravelPackage("Package C", "Pulau Ketam", 100.00)
-    package4 = TravelPackage("Package D","Genting Highlands",350.00)
-    package5 = TravelPackage("Package E","Cameron Highlands",300.00)
-    ]
-   
-def view_cart(self):
-        if not self.cart_items:
-            print("Your cart is empty.")
-        else:
-            print("Your cart contains the following packages:")
-            for item in self.cart_items:
-                print(f"- {item.package_name} | Destination: {item.destination} | Price: RM{item.price}")
+    choice = input("Enter your choice (1-6): ")
 
-def add_to_cart(self, package):
-        self.cart_items.append(package)
+    if choice == "1":
+        display_packages()
+    elif choice == "2":
+        view_cart()
+    elif choice == "3":
+        package_name = input("Enter the package name to add to cart: ")
+        add_to_cart(package_name)
+    elif choice == "4":
+        package_name = input("Enter the package name to remove from cart: ")
+        remove_from_cart(package_name)
+    elif choice == "5":
+        make_payment()
+    elif choice == "6":
+        break
+    else:
+        print("Invalid choice. Please try again.")
 
-def remove_from_cart(self, package):
-        self.cart_items.remove(package)
-
-def calculate_total_cost(self):
-        return self.num_travelers * self.rate
-
-def calculate_bill(cart):
-        total_cost = self.calculate_total_cost()
-        tax_rate = 0.1  # 10% tax rate
-        tax_amount = total_cost * tax_rate
-        bill_amount = total_cost + tax_amount
-        return bill_amount
-
-def cleanup_system():
-    print("Cleaning up travel management system...")
-
-# Entry point of the program
-if __name__ == "__main__":
-    main()
+print("Thank you for visiting our travel package store!")
