a7be40d137c2ffa84586a861e35a1c26d0b0f80f
BRIAN LEE CHONG MING <1211205395@student.mmu.edu.my>
2023-06-12 03:23:47 +0800

    UPDATED

--- CustomerMenu.py
+++ CustomerMenu.py
@@ -16,11 +16,21 @@ def display_packages(packages):
     print("\n===== List of Travel Packages =====\n")
     if packages:
         for i, package in enumerate(packages, start=1):
-            print(f"{i}. Name: {package['name']}, Destination: {package['destination']}, Price: {package['price']}")
-    else:
-        print("No travel packages found.")
-
-
+            print(f"{i}. Name: {package['name']}, Destination: {package['destination']}, Price per Person(RM): {package['price']}")
+        while True:
+            choice = input("\nEnter the number of the package to add to cart ('1' or '0' to exit): ")
+            if choice.isdigit():
+                choice = int(choice)
+                if choice == 0:
+                    break
+                elif choice <= len(packages):
+                    package = packages[choice - 1]
+                    package_name = package['name']
+                    add_to_cart(package_name)
+                    break
+                else:
+                    print("Invalid choice! Please try again.")
+   
 def view_cart():
     print("\nYour Cart:")
     with open("cart.txt", "r") as file:
@@ -33,34 +43,51 @@ def add_to_cart(package_name):
         file.write(package_name + "\n")
     print("\n"+package_name + " added to cart.")
 
-def remove_from_cart(package_name):
+def remove_from_cart(index):
     with open("cart.txt", "r") as file:
         items = file.readlines()
-    items = [item.strip() for item in items if item.strip() != package_name]
-    with open("cart.txt", "w") as file:
-        file.write("\n".join(items))
-    print("\n"+package_name + " removed from cart.")
+    
+    if 1 <= index <= len(items):
+        removed_package = items[index - 1].strip()
+        items.pop(index - 1)
+        with open("cart.txt", "w") as file:
+            file.write("\n".join(items))
+        print(f"\nPackage '{removed_package}' removed from cart.")
+    else:
+        print("Invalid index. Please try again.")
 
 def process_payment():
     with open("cart.txt", "r") as file:
         items = file.readlines()
 
+    selected_packages = []
     total_price = 0
-    with open("bill.txt", "r") as file:
-        packages = file.readlines()
+
+    with open("travel_packages.txt", "r") as file:
+        packages = ast.literal_eval(file.read())
+
         for package in packages:
-            package_details = package.split(",")
-            package_name = package_details[0].strip()
-            package_price = float(package_details[1].strip())
+            package_name = package['name']
+            package_price = float(package['price'])
+
             if package_name + "\n" in items:
+                selected_packages.append(package)
                 total_price += package_price
 
-    print("Total Price:", total_price)
+    num_persons = int(input("Enter the number of persons: "))
+    total_price *= num_persons
 
-    with open("bill.txt", "w") as file:
-        file.write("")
+    print("\n========== PAYMENT RECEIPT ==========")
+    print(f"\nNumber of Persons: {num_persons}\n")
 
-    print("Payment Successful. Thank you for your purchase!")
+    print("Selected Packages:")
+    for package in selected_packages:
+        print(f"- {package['name']}: RM {package['price']} per person")
+
+    print("\nTotal Price: RM", total_price)
+    print("=====================================")
+
+    print("\nPayment Successful. Thank you for your purchase!")
 
 
 def run_cus():
@@ -78,33 +105,23 @@ def run_cus():
         print("\n====== FANTASTOS TRAVEL AGENCY SYSTEM ======")
         print("\n         1. View Packages")
         print("         2. View Cart")
-        print("         3. Add to Cart")
-        print("         4. Remove from Cart")
-        print("         5. Make Payment")
-        print("         6. Exit")
+        print("         3. Remove from Cart")
+        print("         4. Make Payment")
+        print("         5. Exit")
 
-        choice = input("\nPlease enter your choice ('1' or '2' or '3' or '4' or '5' or '6'): ")
+        choice = input("\nPlease enter your choice ('1' or '2' or '3' or '4' or '5'): ")
 
         if choice == "1":
             display_packages(packages)
         elif choice == "2":
             view_cart()
         elif choice == "3":
-            package_name = input("\nEnter the package code to add to cart (EXP:Package A): ")
-            add_to_cart(package_name)
-        elif choice == "4":
-            package_name = input("\nEnter the package code to remove from cart (EXP:Package A): ")
-            remove_from_cart(package_name)
+            index = int(input("\nEnter the index of the package to remove from cart: "))
+            remove_from_cart(index)
+        elif choice == "4": 
+            process_payment()
+            exit(0)
         elif choice == "5":
-            cart_items = []
-            with open("cart.txt", "r") as file:
-                cart_items = file.readlines()
-            if len(cart_items) == 0:
-                print("\nYOUR CART IS EMPTY !")
-            else: 
-                process_payment()
-                exit(0)
-        elif choice == "6":
             break
         else:
             print("\nInvalid Choice! Please try again.")
