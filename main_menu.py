5da8c632159cc3e0de1a2abdc3260d4ac3ecb8aa
BRIAN LEE CHONG MING <1211205395@student.mmu.edu.my>
2023-06-18 23:45:14 +0800

    Updated

--- main_menu.py
+++ main_menu.py
@@ -2,15 +2,14 @@ import login_signin
 import admin
 import CustomerMenu
 
-def user_displayMenu(): #这边是改user menu 的
+def user_displayMenu(): 
     CustomerMenu.run_cus()
 
 
 def admin_displayMenu():
     admin.main()
 
-
-def main(choice): #这里是main
+def main(choice): 
     
     if(choice == 1):
         user_displayMenu()
